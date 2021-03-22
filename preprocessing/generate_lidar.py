5dbac03edb2f1c136f64b1f0bb87a22992b28519
Márk Kaposvári <xmarko45@gmail.com>
2021-03-22 19:21:17 +0100

    Fix scipy import in generate_lidar.py

--- preprocessing/generate_lidar.py
+++ preprocessing/generate_lidar.py
@@ -3,8 +3,9 @@ import os
 
 import numpy as np
 import scipy.misc as ssc
-
+import cv2
 import kitti_util
+from matplotlib.pyplot import imread
 
 
 def project_disp_to_points(calib, disp, max_high):
@@ -60,7 +61,9 @@ if __name__ == '__main__':
         calib = kitti_util.Calibration(calib_file)
         # disp_map = ssc.imread(args.disparity_dir + '/' + fn) / 256.
         if fn[-3:] == 'png':
-            disp_map = ssc.imread(args.disparity_dir + '/' + fn)
+            # disp_map = ssc.imread(args.disparity_dir + '/' + fn)
+            # disp_map = cv2.imread(args.disparity_dir + '/' + fn)
+            disp_map = imread(args.disparity_dir + '/' + fn)
         elif fn[-3:] == 'npy':
             disp_map = np.load(args.disparity_dir + '/' + fn)
         else:
